01a940bd6c560001a55c42854a9fe2b90b2ceb05
Mirajane <151534082+MaryShust@users.noreply.github.com>
2024-11-23 12:57:05 +0300

    Update grahic.asm

--- grahic.asm
+++ grahic.asm
@@ -1,5 +1,6 @@
 section .data
     fmt db "%.2f", 10, 0  ; формат для вывода числа с плавающей точкой
+    inputBuffer resb 20    ; буфер для ввода числа
     two dq 2.0
     neg_two dq -2.0
     neg_half dq -0.5
@@ -10,28 +11,37 @@ section .data
     neg8 dq -8.0
 
 section .bss
-    num resb 20           ; резервируем 20 байт для ввода числа
     result resq 1         ; резервируем 8 байт для результата
 
 section .text
     extern printf
+    extern strtod
     global main
 
 main:
     ; Читаем число с консоли
     mov eax, 3             ; syscall: sys_read
     mov ebx, 0             ; файл: stdin
-    mov ecx, num           ; буфер для хранения числа
+    mov ecx, inputBuffer   ; буфер для хранения числа
     mov edx, 20            ; максимальное количество байт для чтения
     int 0x80               ; вызов ядра
 
-    ; Определяем диапазон и вычисляем y
-    fst st0                ; сохраняем значение в st0 для дальнейших вычислений
+    ; Преобразуем строку в число с плавающей точкой
+    mov rdi, inputBuffer   ; указатель на строку с числом
+    xor rax, rax           ; обнуляем rax для вызова strtod
+    call strtod            ; вызов strtod
+    fstp qword [result]    ; сохраняем результат
+
+    ; Загружаем число из result в FPU
+    fld qword [result]     ; загружаем результат в стек FPU
+
+    ; Проверяем диапазоны и вычисляем y
+    fld st0                ; сохраняем значение в st0 для дальнейших вычислений
     fld st0                ; дублируем значение
-    fadd qword [neg8]   ; x + 8
+    fadd qword [neg8]      ; x + 8
     fmul st0, st0          ; (x + 8)^2
-    fsub qword [four]      ; 4 - 4 * (x + 8)^2
-    fsqrt                   ; sqrt(4 - 4(x + 8)^2)
+    fsub qword [four]      ; 4 - (x + 8)^2
+    fsqrt                   ; sqrt(4 - (x + 8)^2)
 
     ; Проверяем диапазоны
     fld st1                ; загружаем значение x снова
@@ -47,7 +57,7 @@ main:
 below_neg6:
     fld st1                ; загружаем x снова
     fadd qword [neg8]      ; (x + 8)
-    fmul st0               ; 2 + sqrt(4 - 4(x + 8)^2)
+    fmul st0               ; 2 + sqrt(4 - (x + 8)^2)
     fadd qword [two]       ; 2 + ...
     fld st0                ; дублируем результат
     fdiv qword [two]       ; (2 + sqrt(...))/2
@@ -71,18 +81,13 @@ below_neg4:
     fsub qword [three]     ; y = x - 3
     fstp qword [result]    ; сохраняем результат
 
-    fld qword [two]  ; Использовать просто как пример
-
-    ; После расчетов
-    fstp qword [result]  ; сохраняем результат в памяти
-
-    ; Выводим результат
 print_result:
-    mov rdi, fmt          ; формат
-    mov rsi, [result]     ; передаем результат
-    call printf           ; печатаем результат
+    ; Выводим результат
+    mov rdi, fmt           ; формат
+    movq xmm0, [result]    ; передаем результат в xmm0
+    call printf            ; печатаем результат
 
     ; Завершаем программу
-    mov eax, 1            ; syscall: sys_exit
-    xor ebx, ebx          ; код завершения: 0
-    int 0x80              ; вызов ядра
+    mov eax, 1             ; syscall: sys_exit
+    xor ebx, ebx           ; код завершения: 0
+    int 0x80               ; вызов ядра
