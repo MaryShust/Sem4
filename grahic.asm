0937604abb979ce9d17925e1e689c793bcec1440
Mirajane <151534082+MaryShust@users.noreply.github.com>
2024-11-24 21:32:56 +0300

    Update grahic.asm

--- grahic.asm
+++ grahic.asm
@@ -15,85 +15,76 @@ section .bss
 
 section .text
     extern printf
+    extern strtod
     global main
 
 main:
     ; Читаем число с консоли
-    mov eax, 3             ; syscall: sys_read
-    mov ebx, 0             ; файл: stdin
-    mov ecx, num           ; буфер для хранения числа
-    mov edx, 20            ; максимальное количество байт для чтения
-    int 0x80               ; вызов ядра
-
-    ; Преобразуем строку в число с плавающей точкой (нужна библиотека или своя логика)
-
-    ; Здесь должен быть код для парсинга строки num в число с плавающей точкой
-    ; Для простоты мы ожидаем, что пользователь вводит корректное значение.
-
-    ; Простая обработка - предположим, что вводимых данных достаточно
-    ; и мы их просто сохраняем в той же переменной как десятичное число.
-    ; Следует использовать специализированные функции для преобразования.
-
-    ; (ваша логика обработки переменной num здесь...)
+    mov eax, 0            ; syscall: sys_read
+    mov ebx, 0            ; файл: stdin
+    mov ecx, num          ; буфер для хранения числа
+    mov edx, 20           ; максимальное количество байт для чтения
+    int 0x80              ; вызов ядра
 
     ; Преобразуем строку в число с плавающей точкой
-    ; (предполагается, что число всегда корректное и в правильном формате)
-    fild qword [num]       ; загружаем число в FPU стек
+    ; Используем функцию strtod для преобразования
+    lea rdi, num          ; указатель на строку
+    lea rsi, result       ; указатель на результат
+    call strtod           ; преобразуем строку в double
 
     ; Определяем диапазон и вычисляем y
-    fst st0                ; сохраняем значение в st0 для дальнейших вычислений
-    fld st0                ; дублируем значение
-    fadd qword [neg8]   ; x + 8
-    fmul st0, st0          ; (x + 8)^2
-    fsub qword [four]      ; 4 - 4 * (x + 8)^2
-    fsqrt                   ; sqrt(4 - 4(x + 8)^2)
+    fld qword [result]    ; загружаем число в FPU стек
 
     ; Проверяем диапазоны
-    fld st1                ; загружаем значение x снова
-    fcom qword [neg6]      ; сравниваем с -6
-    fstsw ax               ; сохраняем флаги в ax
-    sahf                   ; загружаем флаги в регистры
-    jc below_neg6          ; если x < -6, перейти
+    fld st0               ; сохраняем значение в st0 для дальнейших вычислений
+    fadd qword [neg8]     ; x + 8
+    fmul st0              ; (x + 8)^2
+    fsub qword [four]     ; 4 - (x + 8)^2
+    fsqrt                  ; sqrt(4 - (x + 8)^2)
+
+    fld st1               ; загружаем значение x снова
+    fcom qword [neg6]     ; сравниваем с -6
+    fstsw ax              ; сохраняем флаги в ax
+    sahf                  ; загружаем флаги в регистры
+    ja below_neg6         ; если x > -6, перейти
 
     ; -6 <= x <= -4
-    fld qword [two]        ; y = 2
+    fld qword [two]       ; y = 2
     jmp print_result
 
 below_neg6:
-    fld st1                ; загружаем x снова
-    fadd qword [neg8]      ; (x + 8)
-    fmul st0               ; 2 + sqrt(4 - 4(x + 8)^2)
-    fadd qword [two]       ; 2 + ...
-    fld st0                ; дублируем результат
-    fdiv qword [two]       ; (2 + sqrt(...))/2
-    fstp qword [result]    ; сохраняем результат
+    fld st1               ; загружаем x снова
+    fadd qword [neg8]     ; (x + 8)
+    fmul st0              ; 2 + sqrt(4 - (x + 8)^2)
+    fadd qword [two]      ; 2 + ...
+    fld st0               ; дублируем результат
+    fdiv qword [two]      ; (2 + sqrt(...))/2
+    fstp qword [result]   ; сохраняем результат
 
     ; Проверяем второй диапазон
-    fld st1                ; загружаем x снова
-    fcom qword [neg4]      ; сравниваем с -4
-    fstsw ax               ; сохраняем флаги в ax
-    sahf                   ; загружаем флаги в регистры
-    jc below_neg4          ; если x < -4, перейти
+    fld st1               ; загружаем x снова
+    fcom qword [neg4]     ; сравниваем с -4
+    fstsw ax              ; сохраняем флаги в ax
+    sahf                  ; загружаем флаги в регистры
+    ja below_neg4         ; если x > -4, перейти
 
 negative_four_to_two:
-    fld st1                ; загружаем x снова
-    fmul qword [neg_half]  ; y = -1/2 * x
-    fstp qword [result]    ; сохраняем результат
+    fld st1               ; загружаем x снова
+    fmul qword [neg_half] ; y = -1/2 * x
+    fstp qword [result]   ; сохраняем результат
     jmp print_result
 
 below_neg4:
-    fld st1                ; загружаем x снова
-    fsub qword [three]     ; y = x - 3
-    fstp qword [result]    ; сохраняем результат
-    jmp print_result
+    fld st1               ; загружаем x снова
+    fsub qword [three]    ; y = x - 3
+    fstp qword [result]   ; сохраняем результат
 
-    ; Выводим результат
 print_result:
     mov rdi, fmt          ; формат
-    mov rsi, [result]     ; передаем результат
+    movq rsi, [result]    ; передаем адрес результата
     call printf           ; печатаем результат
 
     ; Завершаем программу
-    mov eax, 1            ; syscall: sys_exit
-    xor ebx, ebx          ; код завершения: 0
-    int 0x80              ; вызов ядра
+    mov eax, 60           ; syscall: sys_exit
+    xor edi, edi          ; код завершения: 0
+    syscall               ; вызов ядра
